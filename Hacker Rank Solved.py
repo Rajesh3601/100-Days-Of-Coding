8ea0e6f077ec6d604725265e67af69765a8ec011
Rajesh Yadav <44490757+Rajesh3601@users.noreply.github.com>
2020-08-05 09:04:05 +0530

    Update Hacker Rank Solved.py

--- Hacker Rank Solved.py
+++ Hacker Rank Solved.py
@@ -7,3 +7,14 @@ while max(a) == r:
     a.remove(max(a))
 
 print(max(a))
+
+
+# Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+marksheet = []
+for _ in range(0,int(input())):
+    marksheet.append([input(), float(input())])
+
+second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
+
