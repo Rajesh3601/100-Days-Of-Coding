7944be2e303aba3ab73854edc0d5b2e9829d2894
Rajesh Yadav <44490757+Rajesh3601@users.noreply.github.com>
2020-08-05 21:44:46 +0530

    Update Hacker Rank Solved.py

--- Hacker Rank Solved.py
+++ Hacker Rank Solved.py
@@ -9,7 +9,8 @@ while max(a) == r:
 print(max(a))
 
 
-# Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+# Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s)
+# of any student(s) having the second lowest grade.
 
 marksheet = []
 for _ in range(0,int(input())):
@@ -18,3 +19,20 @@ for _ in range(0,int(input())):
 second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
 print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
 
+
+# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+# Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    query_marks = student_marks[query_name]
+    total = (sum(query_marks)/ len(query_marks))
+    print("{0:.2f}".format(total))
+    
+    
